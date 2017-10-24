98334fbba42b688e7570f228c20bef4d59f11efa
AbiemAdillah <Abiem.adillah@gmail.com>
2017-10-24 13:03:25 +0800

    Output Luas Rumah dan Luas 1 Box Parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -7,4 +7,8 @@ int main (){
 	double Lantai_parket = 193*1215;
 	double Luas_1box = Lantai_parket*8/1000000;	
 	double Jml_box_parket, Biaya_parket, Biaya_pasang, Totalbyr;
+	
+	cout <<" Luas Rumah 			: "<<Luas_rumah<<" m2"<<endl;
+	cout <<" Luas 1 Box Parket 		: "<<Luas_1box<<" m2"<<endl;
+	
 }
