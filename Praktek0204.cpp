79216988705c8e0e0ba4e7efe65c1ad836765e9e
AbiemAdillah <Abiem.adillah@gmail.com>
2017-10-24 13:01:08 +0800

    Membuat Variabel

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -6,4 +6,5 @@ int main (){
 	double Luas_rumah = 42;
 	double Lantai_parket = 193*1215;
 	double Luas_1box = Lantai_parket*8/1000000;	
+	double Jml_box_parket, Biaya_parket, Biaya_pasang, Totalbyr;
 }
